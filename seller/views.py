6215ac9f5432a773e0bd1502c8e521190dacdb3a
Hari <vharikrishnan532@gmail.com.com>
2026-02-25 13:34:40 +0530

    some updates in nav

--- seller/views.py
+++ seller/views.py
@@ -11,20 +11,21 @@ def seller_profile_view(request):
         return redirect("login")
 
     return render(request, "seller/sellerprofilepage.html", {"profile": profile})
-@login_required
+
 def seller_bridge(request):
+    user=request.user
+    if user.is_authenticated:
+        if SellerProfile.objects.filter(user=request.user).exists():
+            return redirect("seller-profile")
 
-    if SellerProfile.objects.filter(user=request.user).exists():
-        return redirect("seller-profile")
-
-    if request.method == "POST":
-        store_name = request.POST.get("store_name")
-        gst_number = request.POST.get("gst_number")
-        pan_number = request.POST.get("pan_number")
-        bank_account_number = request.POST.get("bank_account_number")
-        ifsc_code = request.POST.get("ifsc_code")
-        business_address = request.POST.get("business_address")
-        store_image = request.FILES.get("store_image")
+        if request.method == "POST":
+            store_name = request.POST.get("store_name")
+            gst_number = request.POST.get("gst_number")
+            pan_number = request.POST.get("pan_number")
+            bank_account_number = request.POST.get("bank_account_number")
+            ifsc_code = request.POST.get("ifsc_code")
+            business_address = request.POST.get("business_address")
+            store_image = request.FILES.get("store_image")
     return render(request, "seller_templates/seller_bridge.html")
 
 
