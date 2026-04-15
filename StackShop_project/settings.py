ad3d9304023dd710b0d4ab7aea90b012a3101b61
Hari <vharikrishnan532@gmail.com.com>
2026-04-15 17:08:43 +0530

    site id sett

--- StackShop_project/settings.py
+++ StackShop_project/settings.py
@@ -66,7 +66,7 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'local')
 DOMAIN = os.getenv('DOMAIN')
 
-SITE_ID = 7
+SITE_ID = int(os.getenv('SITE_ID', 1))
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
